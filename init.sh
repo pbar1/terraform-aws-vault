72e77b257674a7e969032fd9f80dd7a1dc28da3e
Pierce Bartine <pbartine@salesforce.com>
2019-04-12 22:24:29 -0700

    Bash init loop

--- init.sh
+++ init.sh
@@ -15,9 +15,18 @@ export VAULT_ADDR="$1"
 SSM_PATH_VAULT_ROOT_TOKEN="$2"
 SSM_PATH_VAULT_RECOVERY_KEY_BASE64="$3"
 
-# wait for the cluster to show signs of life
+end=$((SECONDS+300))
+
+while [ $SECONDS -lt $end ]; do
+  if [ "$(vault operator init -status)" -eq 2 ]; then
+    break
+  fi
+done
 
 INIT_JSON="$(vault operator init -recovery-shares=1 -recovery-threshold=1 -format=json)"
 
 ROOT_TOKEN=$(echo "$INIT_JSON" | jq -r '.root_token')
 RECOVERY_KEY_BASE64=$(echo "$INIT_JSON" | jq -r '.unseal_keys_b64[0]')
+
+aws ssm put-parameter --name "$SSM_PATH_VAULT_ROOT_TOKEN" --value "$ROOT_TOKEN" --type SecureString
+aws ssm put-parameter --name "$SSM_PATH_VAULT_RECOVERY_KEY_BASE64" --value "$RECOVERY_KEY_BASE64" --type SecureString
